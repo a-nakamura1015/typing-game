19f0e9a80dec549279603247b8016188c719d91b
a-nakamura1015 <a-nakamura@esm.co.jp>
2022-09-29 16:21:53 +0900

    STEP2:入力した文字がお題と一致しているかをチェックするようにした

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// 画面に表示しているお題
+var question = "ringo";
+// カウントしている文字数
+var charCount = 0;
+
 // お題を表示する
 function showQuestion() {
-  var question = "ringo";
   // 画面上に問題を表示する
   document.getElementById("output").innerHTML = question;
 }
@@ -10,4 +14,14 @@ addEventListener("keydown", keydownFunc);
 // 入力したキーを判定する
 function keydownFunc(event) {
   console.log(event.key);
+  // 入力した文字を取得する
+  var input = event.key;
+  // 画面に表示しているお題を1文字ずつ分割する
+  var chars = question.split("");
+  // 評価をする文字を取得する
+  var char = chars[charCount];
+  if (input == char) {
+    console.log("OK");
+    charCount++;
+  }
 }
